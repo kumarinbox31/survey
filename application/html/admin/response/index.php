f6bc56fe390b7e9ef2a4360d91e0267a48bee630
kumarinbox31 <kumarinbox31@gmail.com>
2023-08-06 13:28:11 +0530

    initial

--- application/html/admin/response/index.php
+++ application/html/admin/response/index.php
@@ -1,14 +1,34 @@
 <?php
 
 $html  = 'Record not found.';
-$get = $this->response->get(['project_vendor_id'=>$post['id'],'status'=>$post['rType']]);
+$get = $this->response->get(['project_vendor_id'=>$project_vendor_id,'status'=>$status]);
 if($get->num_rows()){
     $result = $get->result();
     $html = "
-            <table  class='table table-bordered table-striped'>
+            <table  class='table table-bordered'>
             <thead>
-                <
+                <tr>
+                    <th>#</th>
+                    <th>IP</th>
+                    <th>PID</th>
+                    <th>Status</th>
+                    <th>Timestamp</th>
+                </tr>
             </thead>
+            <tbody>";
+        $i = 1;
+        foreach($result as $row){
+            $html .= "<tr>
+                        <td>$i</td>
+                        <td>$row->ip_address</td>
+                        <td>$row->panlist_id</td>
+                        <td>$row->status</td>
+                        <td>$row->created_at</td>
+                    </tr>";
+        $i++;}
+
+        $html .=   "</tbody>
             </table>
         ";
 }
+echo $html;
